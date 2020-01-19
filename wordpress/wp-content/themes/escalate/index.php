c6ca5d3349c77ab0cc6ca741e38054921a003e73
Art Todorov <art.todorov@me.com>
2020-01-18 18:42:18 -0800

    uploladed featured image, created a thumbnail function

--- wordpress/wp-content/themes/escalate/index.php
+++ wordpress/wp-content/themes/escalate/index.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
-test
+  <?php while(have_posts()) : the_post(); ?>
+    <div class="post">
+      <h2 class="title"><a href="#"><?php the_title(); ?></a></h2>
+      <p class="meta">Posted by <a href="#">Someone</a> on July 08, 2013
+        &nbsp;&bull;&nbsp; <a href="#" class="comments">Comments (64)</a> &nbsp;&bull;&nbsp; <a href="#" class="permalink">Full article</a></p>
+      <div class="entry"> <a href="#" class="image image-full">
+          <?php the_post_thumbnail('full'); ?>
+          </a>
+        <?php the_content('Read More'); ?>
+      </div>
+    </div>
+  <?php endwhile; ?> 
 <?php get_footer(); ?>
